81e1e73c2f25913c2b45ff4f0dbda4b11cc1e4d7
Md. Abul Hasan <50300374+iamshantoo@users.noreply.github.com>
2021-04-06 01:59:22 +0600

    Delete

--- ExpenseApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExpenseappConfig(AppConfig):
-    name = 'ExpenseApp'
--- ExpenseApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.showHomePage),
-    path('signin/', views.showLoginPage, name="signin"),
-    path('signup/', views.showRegistrationPage, name='signup'),
-    path('expenses/', views.showExpenseInfoPage, name='expenseInfo'),
-]
