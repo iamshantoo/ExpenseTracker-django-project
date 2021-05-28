50120ec13c2b16559b7fa821a6474add8fa20d82
Md. Abul Hasan <50300374+iamshantoo@users.noreply.github.com>
2021-05-29 03:14:16 +0600

    Update urls.py

--- AccountsApp/urls.py
+++ AccountsApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'AccountsApp'
 
@@ -13,4 +16,15 @@ urlpatterns = [
     path('password/', views.pass_change, name='pass_change'),
     path('add-picture/', views.add_pro_pic, name='add_pro_pic'),
     path('change-picture/', views.change_pro_pic, name='change_pro_pic'),
+    path('password_reset/', views.password_reset_request, name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
