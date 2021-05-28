7f6db017c0eb5373ba1c327d3aad00f063a76c9c
Md. Abul Hasan <50300374+iamshantoo@users.noreply.github.com>
2021-05-29 03:16:39 +0600

    Update urls.py

--- ExpenseApp/urls.py
+++ ExpenseApp/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('', views.HomePage, name='home_page'),
     path('expenses/', views.addExpenseInfoPage, name='expense_info'),
     path('expenses/info/', views.showExpenseInfoPage, name='show_expense_info'),
+    path('expenses/info/graph', views.graph_view,
+         name='graph_view'),
+    path('<int:id>/', views.delete, name='delete'),
+    path('<int:id>/edit', views.update, name='update'),
 ]
